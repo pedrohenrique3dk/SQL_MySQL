2fe2fc33faf39663a9ee08e6a8c04b7ebe141939
Pedro Henrique Martins Belo <pedrohenriqueunicamp2024@gmail.com>
2024-08-06 11:06:06 -0300

    SQL/MySQL

---
+++ PHP/3 - Projeto-Final PHP/contato.php
@@ -0,0 +1,30 @@
+
+
+<?php 
+    include_once "templates/header.php";
+?>
+
+<body>
+    <?php 
+        $nome = $_GET["nome"] ?? "";
+        $assunto = $_GET["assunto"] ?? "";
+    
+    ?>
+    <div class="container-contato">
+        <div class="form-box">
+            <h1 class="h1">Entre em Contato</h1>
+            <form action="<?= $_SERVER["PHP_SELF"]?>" method="get">
+                <input type="text" name="nome" id="nome" placeholder="Nome" value="<?= $nome ?>">
+                <input type="text" id="email" name ="email" placeholder="E-mail">
+                <input type="text" name="assunto" id="assunto" placeholder="Assunto" value = "<?= $assunto ?>">
+                <textarea name="mensagem" id="mensagem" placeholder="Digite sua mensagem..."></textarea>
+                <button type="submit" name="enviar">Enviar</button>
+            </form>
+        </div>
+    </div>
+</body>
+    
+
+<?php 
+    include_once "templates/footer.php";
+?>
