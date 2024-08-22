f2f274df24bf1c1e3a34a8efc014e008daff7bbc
Pedro Henrique Martins Belo <pedrohenriqueunicamp2024@gmail.com>
2024-08-22 13:25:00 -0300

    SQL/MySQL

--- 04 - SQL/01 - Módulos_Anteriores/Aula57.sql
+++ 04 - SQL/01 - Módulos_Anteriores/Aula57.sql
@@ -8,3 +8,14 @@ DATE_FORMAT(last_update, "%d/%m/%y") AS data FROM actor;
 SELECT last_update, DAY (last_update) 
 AS dia, DAY(ADDDATE(last_update, '5')) 
 AS mais_cinco_dias FROM actor;
+
+/*DAYOFWEEK: retorna o dia da semana relacionada a data da coluna */
+USE sakila;
+SELECT last_update FROM actor;
+SELECT last_update, DAYOFWEEK(last_update) FROM actor;
+
+/*DAYOFYEAR: Retorna o dia do ano de uma determinada data*/
+SELECT last_update, DAYOFYEAR(last_update) FROM actor;
+
+
+
