adbe9ef04011842d9762086e1fc5a385eb5a3a43
Pedro Henrique Martins Belo <pedrohenriqueunicamp2024@gmail.com>
2024-09-04 08:58:37 -0300

    SQL/MySQL

--- 04 - SQL/06 - Stored Procedures/Aula64.sql
+++ 04 - SQL/06 - Stored Procedures/Aula64.sql
@@ -2,4 +2,10 @@
 -- Podemos checar as procedures que estão criadas no nosso sistema
 -- O comando é SHOW PROCEDURE STATUS
 
-SHOW PROCEDURE STATUS
+SHOW PROCEDURE STATUS;
+
+-- Excluindo Procedures 
+-- Podemos também excluir procedures do sistema 
+-- O comando é: DROP PROCEDURE <nome>
+
+DROP PROCEDURE fulName;
